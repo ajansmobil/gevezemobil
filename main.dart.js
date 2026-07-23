c973d9b864727635fa969cece6ca8cb3c75ab763
yunusevgane <yunusevgane@users.noreply.github.com>
2026-07-23 10:08:30 +0000

    Deploy: Flutter web release (yunusevgane/gevezeajansMobil)

--- main.dart.js
+++ main.dart.js
@@ -40932,7 +40932,7 @@ $S:2}
 A.bfE.prototype={
 $1(a){var s=A.f3().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/83675ed27633283e7fc296c8bca22e841224c096/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
 $S:53}
 A.a1K.prototype={
 gD(a){var s=this.a
